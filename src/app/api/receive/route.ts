a8b5e1c042c12be2a5dda38bca47702f0ddb8bbb
Shashwat Singh <shashwat123student@gmail.com>
2024-03-03 17:21:22 +0530

    Backend Done

--- src/app/api/receive/route.ts
+++ src/app/api/receive/route.ts
@@ -2,9 +2,35 @@ import { PrismaClient } from '@prisma/client';
 
 const prisma = new PrismaClient();
 
-export async function GET(request: Request) {
+export async function POST(request: Request) {
 
     const { to, from, amount, blockchain, type, username, firstName, lastName } = await request.json();
+
+    let user = await prisma.user.findUnique({
+            where: { username: username }
+        });
+
+        if (!user) {
+            user = await prisma.user.create({
+                data: {
+                    username: username,
+                    firstName: firstName,
+                    lastName: lastName
+                }
+            });
+        }
+
+        await prisma.transaction.create({
+            data: {
+                to: to,
+                from: from,
+                amount: amount,
+                blockchain: blockchain,
+                type: type,
+                transactionType: 'receive',
+                userId: user.id
+            }
+        });
 
     return Response.json({
         to,
--- src/app/api/send/route.ts
+++ src/app/api/send/route.ts
@@ -1,10 +1,45 @@
-import  {NextResponse} from "next/server";
+import { PrismaClient } from '@prisma/client';
 
-export async function GET(request: Request) {
+const prisma = new PrismaClient();
 
+export async function POST(request: Request) {
 
+    const { to, from, amount, blockchain, type, username, firstName, lastName } = await request.json();
+
+    let user = await prisma.user.findUnique({
+        where: { username: username }
+    });
+
+    if (!user) {
+        user = await prisma.user.create({
+            data: {
+                username: username,
+                firstName: firstName,
+                lastName: lastName
+            }
+        });
+    }
+
+    await prisma.transaction.create({
+        data: {
+            to: to,
+            from: from,
+            amount: amount,
+            blockchain: blockchain,
+            type: type,
+            transactionType: 'send',
+            userId: user.id
+        }
+    });
 
     return Response.json({
-        hello: 'world'
+        to,
+        from,
+        amount,
+        blockchain,
+        type,
+        username,
+        firstName,
+        lastName
     })
 }
