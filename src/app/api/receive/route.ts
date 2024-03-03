a8b6f86797c92aa47a3edc01ac065a9dfa4ff8ae
Shashwat Singh <shashwat123student@gmail.com>
2024-03-03 17:03:44 +0530

    Learning backend

--- src/app/api/receive/route.ts
+++ src/app/api/receive/route.ts
@@ -1,7 +1,19 @@
-import  {NextResponse} from "next/server";
+import { PrismaClient } from '@prisma/client';
+
+const prisma = new PrismaClient();
 
 export async function GET(request: Request) {
+
+    const { to, from, amount, blockchain, type, username, firstName, lastName } = await request.json();
+
     return Response.json({
-        hello: 'world'
+        to,
+        from,
+        amount,
+        blockchain,
+        type,
+        username,
+        firstName,
+        lastName
     })
 }
